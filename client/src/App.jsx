dc7c67b5d1375652f488059dedea98b52cde8196
Mariya Rashkova <mrashkkova@gmail.com>
2023-12-07 16:20:28 +0200

    Create MyPurchases component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,6 @@
-// import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-// import * as authService from "./services/authServices";
 import { AuthProvider } from "./contexts/authContext";
 import Path from "./constants/paths";
 
@@ -12,6 +10,7 @@ import Catalogue from "./components/catalogue/Catalogue";
 import ProductDetails from "./components/catalogue/productItem/ProductDetails";
 import AboutUs from "./components/aboutUs/AboutUs";
 import Create from "./components/create/Create";
+import MyPurchases from "./components/myPurchases/myPurchases";
 import Edit from "./components/edit/Edit";
 import Footer from "./components/footer/Footer";
 import Login from "./components/login/Login";
@@ -19,48 +18,6 @@ import Register from "./components/register/Register";
 import Logout from "./components/logout/Logout";
 
 function App() {
-  // const navigate = useNavigate();
-  // const [auth, setAuth] = useState(() => {
-  //   localStorage.removeItem("accessToken");
-
-  //   return {};
-  // });
-
-  // const loginSubmitHandler = async (values) => {
-  //   const result = await authService.login(values.email, values.password);
-
-  //   setAuth(result);
-
-  //   localStorage.setItem("accessToken", result.accessToken);
-
-  //   navigate(Path.Home);
-  // };
-
-  // const registerSubmitHandler = async (values) => {
-  //   const result = await authService.register(values.email, values.password);
-
-  //   setAuth(result);
-
-  //   localStorage.setItem("accessToken", result.accessToken);
-
-  //   navigate(Path.Home);
-  // };
-
-  // const logoutHandler = () => {
-  //   setAuth({});
-
-  //   localStorage.removeItem("accessToken");
-  // };
-
-  // const values = {
-  //   loginSubmitHandler,
-  //   registerSubmitHandler,
-  //   logoutHandler,
-  //   username: auth.username || auth.email,
-  //   email: auth.email,
-  //   isAuthenticated: !!auth.accessToken,
-  // };
-
   return (
     <AuthProvider>
       <Navigation />
@@ -72,6 +29,7 @@ function App() {
         <Route path={Path.Create} element={<Create />} />
         <Route path={Path.Edit} element={<Edit />} />
         <Route path={Path.AboutUs} element={<AboutUs />} />
+        <Route path={Path.MyPurchases} element={<MyPurchases />} />
         <Route path={Path.Login} element={<Login />} />
         <Route path={Path.Register} element={<Register />} />
         <Route path={Path.Logout} element={<Logout />} />
